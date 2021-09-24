75fdad399fac3498e8d7ec8495c0736d3704bb6a
e-manuelb <emanuelcavalcanteb@gmail.com>
2021-09-24 11:19:42 -0300

    Refatoração e reorganização de arquivos.

--- calcme-api/src/main/java/com/calcme/service/UsuarioService.java
+++ calcme-api/src/main/java/com/calcme/service/UsuarioService.java
@@ -1,8 +1,17 @@
 package com.calcme.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.calcme.model.Usuario;
+import com.calcme.repository.UsuarioRepository;
 
-public interface UsuarioService {
-	public Usuario criar(Usuario usuario);
-
+@Service
+public class UsuarioService {
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	public Usuario criar(Usuario usuario) {
+		return this.usuarioRepository.save(usuario);
+	}
 }
